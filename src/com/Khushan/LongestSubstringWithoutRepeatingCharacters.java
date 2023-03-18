704f7bd82431a1827e4d6348aa72e95bc4caae0f
Darshan <darshanshah7135@gmail.com>
2023-03-18 20:50:45 +0530

    new

--- src/com/Khushan/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/Khushan/LongestSubstringWithoutRepeatingCharacters.java
@@ -16,29 +16,55 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-    public int lengthOfLongestSubstring(String s){
-        int length = 0;
-        HashMap<Character,Integer> characterValues = new HashMap<>();
-        ArrayList <Integer> lengthsArrayList = new ArrayList<>();
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        ArrayList<Integer> lengthOfSubstrings = new ArrayList<>();
         int temp = 0;
-        for(int i = 0; i<s.length();i++){
-            if(characterValues.containsKey(s.charAt(i))){
-                lengthsArrayList.add(characterValues.size());
-                characterValues.clear();
+        for(int j=0; j<s.length();j++){
+        for (int i = 0; i < s.length(); i++) {
+            innerloop:
+            if (hashMap.containsKey(s.charAt(i))) {
+                lengthOfSubstrings.add(hashMap.size());
+                hashMap.clear();
                 temp = 0;
+                break innerloop;
             }
-            else{
-                characterValues.put(s.charAt(i),temp);
-                temp++;
-            }
+            hashMap.put(s.charAt(i), temp);
+            temp++;
         }
-        lengthsArrayList.add(characterValues.size());
-        length = Collections.max(lengthsArrayList);
-        return length;
     }
+       // lengthOfSubstrings.add(hashMap.size());
+        int maxLength = Collections.max(lengthOfSubstrings);
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters obj = new LongestSubstringWithoutRepeatingCharacters();
-//        obj.lengthOfLongestSubstring("pwwkew");
-        System.out.println(obj.lengthOfLongestSubstring("pwwkew"));
+      //  obj.lengthOfLongestSubstring("aab");
+        System.out.println(obj.lengthOfLongestSubstring("dvdf"));
     }
 }
+//  same approach with array list
+//        int length = 0;
+//        ArrayList<Character> characters = new ArrayList<>();
+//        ArrayList <Integer> lengthsArrayList = new ArrayList<>();
+//        int temp = 0;
+//        for(int i = 0; i<s.length();i++){
+//            if(characters.contains(s.charAt(i))){
+//                lengthsArrayList.add(characters.size());
+//                characters.clear();
+//                temp = 0;
+//            }
+//            else{
+//                characters.add(s.charAt(i));
+//                temp++;
+//            }
+//        }
+//        lengthsArrayList.add(characters.size());
+//        length = Collections.max(lengthsArrayList);
+//        return length;
+//    }
+//    public static void main(String[] args) {
+//        LongestSubstringWithoutRepeatingCharacters obj = new LongestSubstringWithoutRepeatingCharacters();
+////        obj.lengthOfLongestSubstring("pwwkew");
+//        System.out.println(obj.lengthOfLongestSubstring("pwwkew"));
